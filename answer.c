4acc87d1c6bfc1be0f789a67adedc14b688289d6
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-08-19 20:53:17 +0900

    変数を1文字に

--- answer.c
+++ answer.c
@@ -2,19 +2,19 @@
 
 int main(){
   int n = 0;
-  int sum = 0;
+  int s = 0;
 
   scanf("%d", &n);
 
   for(int i = 1; n > 0; i++){
 
     if( i % 2 != 0 && i % 3 != 0 ){
-      sum += i;
+      s += i;
       n--;
     }
   }
 
-  printf("%d\n", sum);
+  printf("%d\n", s);
 
   return 0;
 }
