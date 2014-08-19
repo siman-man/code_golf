6789c948563699859d2c9dfb4264471092c10195
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-08-19 20:58:29 +0900

    もうちょっときゅっと

--- answer.c
+++ answer.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(){
-int n = 0;
-int s = 0;
-scanf("%d", &n);
-for(int i = 1; n > 0; i++){
-if( i % 2 != 0 && i % 3 != 0 ){
-s += i;
+int n=0;
+int s=0;
+scanf("%d",&n);
+for(int i=1;n>0;i++){
+if(i%2!=0&&i%3!=0){
+s+=i;
 n--;
 }
 }
-printf("%d\n", s);
+printf("%d\n",s);
 return 0;
 }
